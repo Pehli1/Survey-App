82d8f777026174e3805bc2ce600dd35a979c42fd
mertp <mertp@DESKTOP-NT0TABU>
2022-12-31 15:24:36 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sayac = new System.Windows.Forms.Label();
+            this.soru = new System.Windows.Forms.Label();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.radioButtonD = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(132, 104);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 42);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Başla / Cevapla";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Soru:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Skor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cevap:";
+            // 
+            // sayac
+            // 
+            this.sayac.AutoSize = true;
+            this.sayac.Location = new System.Drawing.Point(279, 119);
+            this.sayac.Name = "sayac";
+            this.sayac.Size = new System.Drawing.Size(13, 13);
+            this.sayac.TabIndex = 9;
+            this.sayac.Text = "0";
+            // 
+            // soru
+            // 
+            this.soru.AutoSize = true;
+            this.soru.Location = new System.Drawing.Point(64, 9);
+            this.soru.Name = "soru";
+            this.soru.Size = new System.Drawing.Size(78, 13);
+            this.soru.TabIndex = 10;
+            this.soru.Text = "Kaç yaşındasın";
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(12, 71);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(118, 17);
+            this.radioButtonA.TabIndex = 11;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "17\'den bir fazlasıyım";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(12, 94);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonB.TabIndex = 12;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "18-24";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(12, 117);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonC.TabIndex = 13;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "24-30";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonD
+            // 
+            this.radioButtonD.AutoSize = true;
+            this.radioButtonD.Location = new System.Drawing.Point(12, 140);
+            this.radioButtonD.Name = "radioButtonD";
+            this.radioButtonD.Size = new System.Drawing.Size(49, 17);
+            this.radioButtonD.TabIndex = 14;
+            this.radioButtonD.TabStop = true;
+            this.radioButtonD.Text = "30++";
+            this.radioButtonD.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 173);
+            this.Controls.Add(this.radioButtonD);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.soru);
+            this.Controls.Add(this.sayac);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Name = "Form1";
+            this.Text = "Mert Pehlivan - Questions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label sayac;
+        private System.Windows.Forms.Label soru;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.RadioButton radioButtonD;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            //Soruların ve Cevapların sırayla gelmesi.
+        string[] Sorular = {"Favori futbolcun", " En sevdiğin araba markası", "Ziyaret etmek istediğin ülkeyi seç", "Favori takımın", "Yaşamak istediğin yüzyıl", "Sorular BİTTİ uygulamayı TERKET !!!" };
+        static int s = 0;
+
+        string[] CevapA = { "Messi", "Mercedes Maybach", "İngiltere", "zonguldak kömürleri", "100-200'ler", "TERKET" };
+        static int a = 0;
+
+        string[] CevapB = { "PEnaldo", "BMW M3 2016 Sport", "İzlanda", "Beşiktaş", "1500'ler", "TERKET" };
+        static int b = 0;
+
+        string[] CevapC = { "Sergen Yalçın", " Range Rover", "İspanya", "giresun spor", "1800'ler", "TERKET" };
+        static int c = 0;
+
+
+        string[] CevapD = { "Cenk tosun", " McLaren P1 ", "İtalya", "erdek SK", "2000'ler", "TERKET" };
+        static int d = 0;
+        //Soruların ve Cevapların sırayla gelmesi.
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+                if (radioButtonA.Checked || radioButtonB.Checked || radioButtonC.Checked || radioButtonD.Checked)
+                {
+
+                    soru.Text = Sorular[s];
+                    s = (s + 1);
+
+                    radioButtonA.Text = CevapA[a];
+                        a = (a + 1);
+
+                    radioButtonB.Text = CevapB[b];
+                        b = (b + 1);
+
+                    radioButtonC.Text = CevapC[c];
+                        c = (c + 1);
+
+                    radioButtonD.Text = CevapD[d];
+                        d = (d + 1);
+            }
+            else
+            {
+                MessageBox.Show("Cevap seçiniz.");  //Soru kontrol fonksiyonu.
+            }
+
+            
+
+                //Skorun arttırılması.
+            var curr = 0;
+            if (int.TryParse(sayac.Text, out curr))
+                curr++;
+            sayac.Text = curr.ToString();
+            //Skorun arttırılması.
+
+        }
+    }
+}
